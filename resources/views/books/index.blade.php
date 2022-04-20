76a4aa3497fba24531c6cdc54d365d6bd59a604b
pedro <pedrocampota@outook.com>
2022-04-20 18:02:30 +0100

    Fase 5 - Concluida
    A faltar fica:
    - Falta corrigir um bug. Este ocorre quando se tenta adicionar diferentes livros à wishlist.
    - Falta melhorar o responsive design da grid dos books tanto da página inicial como na página da wishlist.

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -3,13 +3,13 @@
         <div class="flex">
             <x-categories-sidebar></x-categories-sidebar>
 
-            <div class="block">
+            <div class="block" style="margin: 0 auto;">
                 @if ($books->count())
                     <x-books-grid :books="$books" :categories="$categories"></x-books-grid>
 
                     {{ $books->links() }}
                 @else
-                    <div style="margin: 0 auto;" class="flex items-center">
+                    <div style="margin: 0 auto; margin-top: 100px" class="flex items-center">
                         <div style="text-align:center;margin: 0 auto; border-radius: 12px; border: 1px solid #cccccc; padding: 35px">
                             <h1 style="margin-bottom: 10px"><i class="fa-solid fa-face-grimace" style="margin-right: 10px"></i>Oops!</h1>
                             <p style="font-size: 16px">No books yet. Please check back later.</p>
